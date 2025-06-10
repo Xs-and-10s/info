f0da8704f8748a21cc07ffab66560b0a794354c7
Mark Manley <mark.manley3@icloud.com>
2025-06-10 14:27:53 -0400

    fix base path config

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/Xs-and-10s.github.io",
+  basePath: "/info",
 
   /**
    * Disable server-based image optimization. Next.js does not support
